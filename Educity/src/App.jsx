d41d95803277a03cff628f5dd49afdfea1388eca
Saiganesh-ctk <saiganeshctk@gmail.com>
2025-10-25 18:38:22 +0530

    Adding hero component

--- Educity/src/App.jsx
+++ Educity/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/navbar/navbar";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
     <div>
       <Navbar></Navbar>
+      <Hero />
     </div>
   );
 }
