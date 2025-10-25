723cbfc8fc2e71586a95ed8d2df768b208927d29
Saiganesh-ctk <saiganeshctk@gmail.com>
2025-10-25 10:58:08 +0530

    Second Commit - creating navbar

---
+++ Educity/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <img
+        src={logo}
+        alt=""
+      />
+      <ul>
+        <li>Home</li>
+        <li>Program</li>
+        <li>About us</li>
+        <li>Campus</li>
+        <li>Testimonial</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
