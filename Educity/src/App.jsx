a9debefeb3677af2481f0214e3cfeff978e77fed
Saiganesh-ctk <saiganeshctk@gmail.com>
2025-10-25 21:30:28 +0530

    Added Programs component

--- Educity/src/App.jsx
+++ Educity/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "./components/navbar/navbar";
 import Hero from "./components/Hero/Hero";
+import Programs from "./components/Programs/Programs";
 
 function App() {
   return (
     <div>
-      <Navbar></Navbar>
+      <Navbar />
       <Hero />
+      <div className="container">
+        <Programs />
+      </div>
     </div>
   );
 }
