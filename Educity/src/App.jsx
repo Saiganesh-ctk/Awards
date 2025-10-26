f481050b91367895d54526f81b9a48d265811ccc
Saiganesh-ctk <saiganeshctk@gmail.com>
2025-10-26 17:08:21 +0530

    Added Campus component

--- Educity/src/App.jsx
+++ Educity/src/App.jsx
@@ -4,6 +4,7 @@ import Hero from "./components/Hero/Hero";
 import Programs from "./components/Programs/Programs";
 import Title from "./components/Title/Title";
 import About from "./components/About/About";
+import Campus from "./components/Campus/Campus";
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
           subtitle="Gallery"
           title="Campus Photos"
         />
+        <Campus />
       </div>
     </div>
   );
